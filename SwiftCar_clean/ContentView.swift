e3b852eb832a952a4d4cebe4cbf1feb0c4da6760
hellosaumil <6513922+hellosaumil@users.noreply.github.com>
2019-12-23 01:26:29 -0800

    Initial Commit

---
+++ SwiftCar_clean/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftCar_clean
+//
+//  Created by Saumil Shah on 12/23/19.
+//  Copyright © 2019 Saumil Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
